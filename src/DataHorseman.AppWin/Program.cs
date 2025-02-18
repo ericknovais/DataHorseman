f9d32ea6fc3faad82e3630e1928316b0c0c003e9
Da Hora <47401961+ericknovais@users.noreply.github.com>
2025-02-18 00:53:09 -0300

    DH-01

--- src/DataHorseman.AppWin/Program.cs
+++ src/DataHorseman.AppWin/Program.cs
@@ -8,6 +8,8 @@ internal static class Program
     [STAThread]
     static void Main()
     {
+        //var services = new ServiceCollection();
+        //services.AddScoped<IService, Service>();
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
